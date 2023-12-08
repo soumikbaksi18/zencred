a4c0900c5c515f0fefad8103a5861bde76f47d7d
swetakarar <kararsweta@gmail.com>
2023-12-09 03:50:08 +0530

    Frontend and Backend connected

--- src/App.js
+++ src/App.js
@@ -15,48 +15,8 @@ import TokenMaster from "./abis/TokenMaster.json";
 import config from "./config.json";
 
 const App = () => {
-  const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
 
-  const [tokenMaster, setTokenMaster] = useState(null);
-  const [occasions, setOccasions] = useState([]);
-
-  const [occasion, setOccasion] = useState({});
-
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    setProvider(provider);
-
-    const network = await provider.getNetwork();
-    const tokenMaster = new ethers.Contract(
-      config[network.chainId].TokenMaster.address,
-      TokenMaster,
-      provider
-    );
-    setTokenMaster(tokenMaster);
-
-    const totalOccasions = await tokenMaster.totalOccasions();
-    const occasions = [];
-
-    for (var i = 1; i <= totalOccasions; i++) {
-      const occasion = await tokenMaster.getOccasion(i);
-      occasions.push(occasion);
-    }
-
-    setOccasions(occasions);
-
-    window.ethereum.on("accountsChanged", async () => {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      const account = ethers.utils.getAddress(accounts[0]);
-      setAccount(account);
-    });
-  };
-
-  useEffect(() => {
-    loadBlockchainData();
-  }, []);
   return (
     <>
       <div>
@@ -65,17 +25,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/verify" element={<Verify />} />
-        <Route
-          path="/event"
-          element={
-            <Event
-              occasion={occasion}
-              tokenMaster={tokenMaster}
-              provider={provider}
-              account={account}
-            />
-          }
-        />
+        <Route path="/event" element={<Event />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/carbon" element={<Carbon />} />
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 import hero1 from "../assets/herobox1.svg";
 import hero2 from "../assets/herobox2.svg";
@@ -8,7 +9,53 @@ import c2 from "../assets/cutie2.svg";
 import hand from "../assets/hand.svg";
 import Events from "../components/Events";
 
+// ABIs
+import TokenMaster from "../abis/TokenMaster.json";
+
+// Config
+import config from "../config.json";
+
 const Home = () => {
+  const [tokenMaster, setTokenMaster] = useState(null);
+  const [occasions, setOccasions] = useState([]);
+  const [provider, setProvider] = useState(null);
+  const [occasion, setOccasion] = useState({});
+  const [toggle, setToggle] = useState(false);
+  const [account, setAccount] = useState(null);
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+    const tokenMaster = new ethers.Contract(
+      config[network.chainId].TokenMaster.address,
+      TokenMaster,
+      provider
+    );
+    setTokenMaster(tokenMaster);
+
+    const totalOccasions = await tokenMaster.totalOccasions();
+    const occasions = [];
+
+    for (var i = 1; i <= totalOccasions; i++) {
+      const occasion = await tokenMaster.getOccasion(i);
+      occasions.push(occasion);
+    }
+
+    setOccasions(occasions);
+
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
   return (
     <>
       <div>
@@ -58,7 +105,13 @@ const Home = () => {
       </div>
 
       <div>
-        <Events />
+        <Events
+          occasions={occasions}
+          setOccasion={setOccasion}
+          tokenMaster={tokenMaster}
+          provider={provider}
+          account={account}
+        />
       </div>
 
       <div className="mb-6">
